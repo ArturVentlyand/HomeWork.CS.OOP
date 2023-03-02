93770b50a70cecf9a979296e791cc1981e4243a4
Artur Ventlyand <113329020+ArturVentlyand@users.noreply.github.com>
2023-03-02 08:16:27 +0200

    Add files via upload

---
+++ HomeWork2/Task4/Task4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task4 
+{ 
+    internal class Program
+    {
+        struct Dog
+        {
+            public string Name;
+            public string Mark;
+            public int Age;
+
+            public override string ToString()
+            {
+                return String.Format($"\nName - {Name}; \nMark - {Mark}, \nAge - {Age}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Dog myDog;
+            Console.Write("Enter a name your dog: ");
+            myDog.Name = Console.ReadLine();
+
+            Console.Write("Enter a mark your dog: ");
+            myDog.Mark = Console.ReadLine();
+            
+            Console.Write("Enter a age your dog: ");
+            myDog.Age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Dog info: {myDog}");
+        }
+    }
+}
