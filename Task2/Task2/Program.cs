56db891f67928e498fe323cf46f33cd788b2b1a1
Artur Ventlyand <113329020+ArturVentlyand@users.noreply.github.com>
2023-03-02 08:08:16 +0200

    Add files via upload

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a first number: ");
+            int firstNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter a second number: ");
+            int secondNum = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Enter a third number: ");
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            int max, min;
+            max = firstNum > secondNum ? firstNum : secondNum;
+            if (max < thirdNum)
+                max = thirdNum;
+
+            min = firstNum < secondNum ? firstNum : secondNum;
+            if (min > thirdNum)
+                min = thirdNum;
+
+            Console.WriteLine($"max = {max}, min = {min}");
+        }
+    }
+}
