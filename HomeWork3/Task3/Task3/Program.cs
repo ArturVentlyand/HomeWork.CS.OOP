64ba4070c04f8393e650a3a757e6fc28a941203c
Artur Ventlyand <113329020+ArturVentlyand@users.noreply.github.com>
2023-03-04 15:07:16 +0200

    Add files via upload

---
+++ HomeWork3/Task3/Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a name of the drink: ");
+            string nameDrink = Console.ReadLine();
+            double priceDrink = 0;
+
+            switch (nameDrink.ToLower())
+            {
+                case "tea":
+                case "coffee":
+                    priceDrink = 1.5;
+                    Console.WriteLine($"{nameDrink} = ${priceDrink}");
+                    break;
+                case "juice":
+                    priceDrink = 3.0;
+                    Console.WriteLine($"{nameDrink} = ${priceDrink}");
+                    break;
+                case "water":
+                    priceDrink = 2.5;
+                    Console.WriteLine($"{nameDrink} = ${priceDrink}");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
