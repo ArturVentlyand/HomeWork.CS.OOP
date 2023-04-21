1c5ebb6194370472ab0d4db3a698af881383b88b
Artur Ventlyand <113329020+ArturVentlyand@users.noreply.github.com>
2023-04-21 10:02:51 +0300

    Add files via upload

---
+++ HomeWork12/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection.Metadata;
+using System.Xml.Serialization;
+
+namespace HomeWork12 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> shapes = new List<Shape>();
+            shapes.Add(new Circle("C00", 2));
+            shapes.Add(new Circle("C01", 2.5));
+            shapes.Add(new Square("S00", 2));
+            shapes.Add(new Square("S01", 2.5));
+
+            Circle circle = new Circle("C02", 3);
+            circle.Serialize("C:\\Users\\ventl\\OneDrive\\Рабочий стол\\Projects\\Study.CS.OOP\\HomeWork12\\Test.bin");
+            Shape.Deserialize("C:\\Users\\ventl\\OneDrive\\Рабочий стол\\Projects\\Study.CS.OOP\\HomeWork12\\Test.bin");
+            
+
+            var serializer = new XmlSerializer(typeof(List<Shape>));
+
+            using (var stream = new FileStream("C:\\Users\\ventl\\OneDrive\\Рабочий стол\\Projects\\Study.CS.OOP\\HomeWork12\\XMLFile1.xml", FileMode.Create))
+            {
+                serializer.Serialize(stream, shapes);
+            }
+        }
+    }
+}
---
+++ HomeWork12/Shape.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace HomeWork12
+{
+    [Serializable]
+    [XmlInclude(typeof(Circle))]
+    [XmlInclude(typeof(Square))]
+    public abstract class Shape : IComparable<Shape>
+    {
+        [XmlElement]
+        private string name;
+        public string Name { get => name; set => name = value; }
+
+        public Shape() {}
+        public Shape(string name)
+        {
+            Name = name;
+        }
+
+        public abstract double Area();
+
+        public abstract double Perimeter();
+
+        public virtual void Display()
+        {
+            Console.WriteLine($"Name - {Name} \n" +
+                              $"Area = {Area()} \n" +
+                              $"Perimeter = {Perimeter()}");
+        }
+        public virtual void DisplayArea()
+        {
+            Console.WriteLine($"Area = {Area()}");
+        }
+
+        public virtual int CompareTo(Shape other)
+        {
+            return Area().CompareTo(other.Area());
+        }
+
+        public void Serialize(string fileName)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream stream = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                formatter.Serialize(stream, this);
+            }
+        }
+
+        public static Shape Deserialize(string fileName)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                return (Shape)formatter.Deserialize(stream);
+            }
+
+        }
+    }
+}
